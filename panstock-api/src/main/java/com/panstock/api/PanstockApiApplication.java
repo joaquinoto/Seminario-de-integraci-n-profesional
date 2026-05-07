f3b832e56c45b5fc030b5925df0657baeae65f6a
Ramiro Abadie <rama.abadie@gmail.com>
2026-05-07 15:59:50 -0300

    Agrega estructura inicial backend Spring Boot
    Agregue carpeta de backend creada con springboot

---
+++ panstock-api/src/main/java/com/panstock/api/PanstockApiApplication.java
@@ -0,0 +1,13 @@
+package com.panstock.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanstockApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanstockApiApplication.class, args);
+	}
+
+}
---
+++ panstock-api/src/test/java/com/panstock/api/PanstockApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panstock.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanstockApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
