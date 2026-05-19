e8c3b66739457951f7826b1af4a6b566acdf18df
Joanna-Lazarte <jlazartelagos@gmail.com>
2026-05-19 14:10:55 -0300

    Actualizacion de eslint.config

--- Front/panstock-client/eslint.config.js
+++ Front/panstock-client/eslint.config.js
@@ -1,21 +1,42 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default [
+  { ignores: ['dist'] },
   {
     files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
+      ecmaVersion: 2020,
       globals: globals.browser,
-      parserOptions: { ecmaFeatures: { jsx: true } },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
     },
-  },
-])
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      'react/jsx-no-target-blank': 'off',
+      'react-refresh/only-export-components': 'off',
+    
+      // New rule to throw an error for undefined variables (not imported)
+      'no-undef': 'error',
+
+      // Disable prop-types validation
+      'react/prop-types': 'off',
+
+      // Disable warnings/errors for unused variables
+      'no-unused-vars': 'off'
+    }
+  }
+];
